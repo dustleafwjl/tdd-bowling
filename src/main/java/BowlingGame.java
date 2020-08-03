9d22d4d83d26c592d7c7d65bed612f3df1218fa3
dustleafwjl <wangjianglin_priv@qq.com>
2020-08-03 20:31:30 +0800

    [jianglin wang] feat: finished test

---
+++ src/main/java/BowlingGame.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class BowlingGame {
+    public int getLineScore(String scoreCode) {
+        int score = calculate(scoreCode);
+        return 300;
+    }
+
+    private int calculate(String scoreCode) {
+//        ArrayList scoreArr = scoreCode.split("|");
+        return 0;
+    }
+}
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -1,2 +1,25 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class BowlingGameTest {
+    @Test
+    public void should_get_sum_when_getlinescore_given_without_strike_and_spare() {
+        BowlingGame bowlingGame = new BowlingGame();
+        Assertions.assertEquals(50, bowlingGame.getLineScore("5-4|5-4|5-4|5-4|5-4|5-4|5-4|5-4|5-4|5-4"));
+    }
+    @Test
+    public void should_get_sum_when_getlinescore_given_all_strike() {
+        BowlingGame bowlingGame = new BowlingGame();
+        Assertions.assertEquals(300, bowlingGame.getLineScore("A|A|A|A|A|A|A|A|A|A|A"));
+    }
+    @Test
+    public void should_get_150_when_getlinescore_given_all_spare() {
+        BowlingGame bowlingGame = new BowlingGame();
+        Assertions.assertEquals(150, bowlingGame.getLineScore("5-5|5-5|5-5|5-5|5-5|5-5|5-5|5-5|5-5|5-5|5"));
+    }
+    @Test
+    public void should_sum_when_getlinescore_given_type_mixed() {
+        BowlingGame bowlingGame = new BowlingGame();
+        Assertions.assertEquals(254, bowlingGame.getLineScore("A|A|A|A|A|A|A|A|5-5|5-4"));
+    }
 }
